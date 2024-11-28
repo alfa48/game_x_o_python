8cdbb365c8a19066960e7d016c7c66edea0be36d
alfa48 <948manuel@gmail.com>
2024-11-28 22:54:36 +0100

    cursor funcional

--- game_x_o.py
+++ game_x_o.py
@@ -9,62 +9,58 @@ programa sob licença GNU v3
 from curses import initscr, wrapper
 from random import randint
 
-def boas_vindas(stdscr):
+def welcome_screen(stdscr):
     stdscr.clear()
     stdscr.addstr(1, 1, 'Bem vindo ao jogo X_O.')
     stdscr.addstr(2, 1, 'Pressione q para sair e h para obter ajuda.')
-    stdscr.addstr(16, 1, 'KUKINA_tech')
-    stdscr.addstr(17, 1, 'by Manuel Andre')
+    stdscr.addstr(16, 1, 'Create by Manuel Andre')
     stdscr.refresh()
 
-
-def reiniciar_tela(stdscr, limpar=True):
-    if limpar is True:
+def init_screen(stdscr, clean=True):
+    if clean is True:
         stdscr.clear()
     stdscr.border()
-    boas_vindas(stdscr)
+    welcome_screen(stdscr)
     stdscr.refresh()
 
-def draw_tabuleiro(stdscr, posicoes, X_center):
+def draw_table(stdscr, posicoes, X_center):
     stdscr.clear()
-    reiniciar_tela(stdscr, limpar=False)
+    init_screen(stdscr, clean=False)
 
     stdscr.addstr(10, X_center - 3, '------')
     stdscr.addstr(12, X_center - 3, '------')
     i = 9
-    for linha in posicoes:
-        tela = '%s|%s|%s' % tuple(linha)
-        stdscr.addstr(i, X_center - 3, tela)
+    for line in posicoes:
+        screen = '%s|%s|%s' % tuple(line)
+        stdscr.addstr(i, X_center - 3, screen)
         i += 2
 
 def limites(pos_x, pos_y):
     if pos_x > 2:
         pos_x = 0
-    if pos_x < 0:
+    elif pos_x < 0:
         pos_x = 2
 
-    if pos_y > 2:
+    elif pos_y > 2:
         pos_y = 0
-    if pos_y < 0:
+    elif pos_y < 0:
         pos_y = 2
     return (pos_x, pos_y)
 
-def espaco_do_tabuleiro(pos_x, pos_y, entrada):
-    if entrada == 'a':
+def space_of_table(pos_x, pos_y, key):
+    if key == 'a':
         pos_x, pos_y = limites(pos_x - 1, pos_y)
-    if entrada == 'd':
+    elif key == 'd':
         pos_x, pos_y = limites(pos_x + 1, pos_y)
-    if entrada == 'w':
+    elif key == 'w':
         pos_x, pos_y = limites(pos_x, pos_y - 1)
-    if entrada == 's':
-        pos_x, pos_y = limites(pos_x - 1, pos_y + 1)
-    else:
-        pass
+    elif key == 's':
+        pos_x, pos_y = limites(pos_x, pos_y + 1)
     return (pos_x, pos_y)
-    
-def cursor(stdscr, pos_x, pos_y, X_center):
+
+def cursor(stdscr, pos_x, pos_y, x_center):
     cursor_y = 9
-    cursor_x = X_center - 3
+    cursor_x = x_center - 3
     if pos_x == 1:
         cursor_x += 2
     if pos_x == 2:
@@ -73,10 +69,11 @@ def cursor(stdscr, pos_x, pos_y, X_center):
         cursor_y += 2
     if pos_y == 2:
         cursor_y += 4
-    stdscr.move(cursor_x, cursor_y)
+    #print("X: {0} Y: {1}".format(cursor_x, cursor_y))
+    stdscr.move(cursor_y, cursor_x)
 
 
-def ajuda(stdscr):
+def help(stdscr):
         stdscr.clear()
         stdscr.addstr(1, 1, 'Para mudar a posicao, use as teclas A, D, S, W')
         stdscr.addstr(2, 1, 'Para definir uma posicao, use a tecla L')
@@ -88,24 +85,25 @@ def ajuda(stdscr):
 
 
 def main(stdscr):
-    
-    reiniciar_tela(stdscr)
+
+    init_screen(stdscr)
     width = stdscr.getmaxyx()[1]
-    X_center = (width - 1) // 2
-    posicoes = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    x_center = (width) // 2
+    position = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     pos_x, pos_y = 0, 0
 
     while (True):
-        tecla = stdscr.getkey()
-        if tecla == 'q':
+        key = stdscr.getkey()
+        if key == 'q':
             break
-        if (tecla in ['a', 'd', 'w', 's']):
-            pos_x, pos_y = espaco_do_tabuleiro(pos_x, pos_x, tecla)
-        if tecla == 'h':
-            ajuda(stdscr)
+        if (key in ['a', 'd', 'w', 's']):
+            pos_x, pos_y = space_of_table(pos_x, pos_y, key)
+        if key == 'h':
+            help(stdscr)
         else:
-            draw_tabuleiro(stdscr, posicoes, X_center)
-            cursor(stdscr, pos_x, pos_y, X_center)
+            draw_table(stdscr, position, x_center)
+            #stdscr.move(pos_x, pos_y)
+            cursor(stdscr, pos_x, pos_y, x_center)
 
         #pass
 
